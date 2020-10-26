cc1b428d0ec932b7e515e1bc9d8bb19b4dad1115
Brian Seek <brian.seek@civicactions.com>
2020-10-26 15:01:28 -0700

    Cleaned up drush command.

--- src/Commands/EmbeddedBlockCommands.php
+++ src/Commands/EmbeddedBlockCommands.php
@@ -3,7 +3,7 @@
 namespace Drupal\embedded_block_generator\Commands;
 
 use Drush\Commands\DrushCommands;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\File\FileSystemInterface;
 
 /**
  * A drush command file.
@@ -11,7 +11,7 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
  * @package Drupal\embedded_block_generator\Commands
  */
 class EmbeddedBlockCommands extends DrushCommands {
-  use StringTranslationTrait;
+
   /**
    * Drush command that displays the given text.
    *
@@ -26,29 +26,8 @@ class EmbeddedBlockCommands extends DrushCommands {
     try {
       $path_name = preg_replace('@[^a-z0-9-]+@', '_', strtolower($module_name));
       $path = DRUPAL_ROOT . '/modules/custom/' . $path_name;
-      if (file_prepare_directory($path, FILE_CREATE_DIRECTORY)) {
-
+      if (\Drupal::service('file_system')->prepareDirectory($path, FileSystemInterface::CREATE_DIRECTORY)) {
         $buildmodule->buildModuleFiles($module_name, $path);
-//        $buildmodule->buildRoutingFile($values, $path, $module_name);
-
-
-//        if ($values['default_controller'] == 1) {
-//          $create_controller = $buildmodule->buildController($values, $path, $module_name);
-//        }
-//        if ($values['default_form'] == 1) {
-//          $create_form = $buildmodule->buildForm($values, $path, $module_name);
-//        }
-//        if ($values['default_block'] == 1) {
-//          $create_block = $buildmodule->buildBlock($values, $path, $module_name);
-//        }
-//        chmod($path, 0777);
-//        if ($values['module_option'] == 2) {
-//          module_maker_enable_module($module_name, $name);
-//        }
-//        if (!empty($values['module_option']) && $values['module_option'] == 1) {
-//          module_maker_create_zip($module_name);
-//          drupal_set_message(t('Module "<i> @name </i>" Created successfully', ['@name' => $name]), 'status');
-//        }
       }
       $this->output()->writeln('Module ' . $module_name . ' Created successfully');
     }
@@ -56,8 +35,5 @@ class EmbeddedBlockCommands extends DrushCommands {
       $this->output()->writeln($e->getMessage());
     }
 
-
-
   }
-
 }
